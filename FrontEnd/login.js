9358f5fb700c0876bce1ba174ea8b7115e76f9d7
Servant Clément <clementServant@protonmail.com>
2023-11-24 16:04:22 +0100

    Modification de la response.ok en response.status

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,22 +1,24 @@
 // ! Récupération de l'element <form> depuis le DOM
 const loginForm = document.getElementById("login-form")
 
-// + Ajout event listener sur le bouton SUBMIT
+// + Ajout de l’écouteur d’événement sur le bouton SUBMIT
 loginForm.addEventListener("submit", async function (event) {
 
-    // ! Éviter le rechargement de la page 
+    // ! Éviter le rechargement de la page par défaut.
     event.preventDefault();
 
     // ! Récupération des données input pour l'email et le password
     const email = document.getElementById("email").value 
     const password = document.getElementById("password").value
 
+    // + creation objet "formdata" + donner a récupérer
     const formData = {
       email: email,
       password: password
     }
-
+    // + function asynchrone requête API
     async function fetchUtilisateur () {
+      // ! Méthode fetch requête post a l'API + entête + corp de la requête.
       const response = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {
@@ -25,13 +27,14 @@ loginForm.addEventListener("submit", async function (event) {
         },
         body: JSON.stringify(formData)
       })
-      if (response === 200) {
+      // ! Gestion réponse serveur
+      if (response.status === 200) {
         const data = await response.json()
         const token = data.token
         localStorage.setItem("token", token)
         alert("Connection réussie ! Redirection vers la page d’accueil")
         window.location.href = "index.html"
-      } else (response === 401 || response === 404); {
+      } else (response.status === 401 || response.status === 404); {
         alert("Identifient ou mot de passe incorrect ! Veuillez réessayer");
       }
     }
